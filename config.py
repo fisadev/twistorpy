11a9f7b67e33da6bef9cbc56c76b592b9c6a799a
T0n1 <toni@localhost.localdomain>
2016-11-25 10:56:28 -0300

    initials configs

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+"""
+    To create all keys you need to go:
+    https://apps.twitter.com and select myapps.
+"""
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
