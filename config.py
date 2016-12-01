0c0c5c32a85e81b141b34bd576c7530a8836e775
Juan Pedro Fisanotti <fisadev@gmail.com>
2016-12-01 18:14:35 -0300

    Added default db, and some styling on the config comments

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-"""
-    To create all keys you need to go:
-    https://apps.twitter.com and select myapps.
-"""
-
+# To create all keys you need to go:
+# https://apps.twitter.com and select myapps.
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
-CONNECT_STRING = ''  # Reference: 	http://docs.sqlalchemy.org/en/latest/core/engines.html
+
+# Reference: http://docs.sqlalchemy.org/en/latest/core/engines.html
+CONNECT_STRING = 'sqlite:///tweets.db'
